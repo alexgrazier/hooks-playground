ced005f62dad1b7c5b45797f6dde7330d21b122f
Alex Grazier <kylealexandra@gmail.com>
2021-06-11 16:14:44 -0400

    create counter example with setState and closed over function for activating button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <button onClick={() => setCount(count + 1)}>Add to count</button>
+//       <p>The current count is {count}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// set state of active button to true
+// create state of text that is shown (or prop?)
+// create toggle of button click to change activation
+
 function App() {
+  const [activeButton, setActiveButton] = useState(true);
+  const buttonText = activeButton ? 'I am an active button. Click me to inactivate me' : 'I am an inactive button. Click me to activate me'
+  // use a function for this b/c it uses closure => internal function retains acccess to variables in external scope
+  const toggleButtonActivation = () => {
+    setActiveButton(!activeButton);
+  };
+  console.log('activeButton :', activeButton);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleButtonActivation}>{buttonText}</button>
+      <p>The current active state of the button is {activeButton.toString()}</p>
     </div>
   );
 }
